30e7960cdc11f609598a22c7c108fb8c0d2b1a5a
muffinboy <87519281+muffinboy19@users.noreply.github.com>
2024-07-28 12:41:11 +0530

    new algos added

--- Notes/notes.cpp
+++ Notes/notes.cpp
@@ -29,3 +29,12 @@ here is what you will do
                     henche 8 
                     
 
+
+
+XOR of two same numbers is always 0 i.e. a ^ a = 0. ←Property 1.
+XOR of a number with 0 will result in the number itself i.e. 0 ^ a = a.  ←Property 2
+
+Here all the numbers except the single number appear twice and so 
+will form a pair. Now, if we perform XOR of all elements of the array, 
+the XOR of each pair will result in 0 according to the XOR property 1. 
+The result will be = 0 ^ (single number) = single number (according to property 2).
