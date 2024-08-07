6969a2b5f1d5491522ad5d01112abc10fa3facc8
muffinboy <87519281+muffinboy19@users.noreply.github.com>
2024-08-08 00:23:51 +0530

    this is Varible size window algo

--- Topics/Dynamic Programming/fibonacci.cpp
+++ Topics/Dynamic Programming/fibonacci.cpp
@@ -4,32 +4,32 @@ here there are going to be 3 solutions to per question
 */
 
 
-// // ! this Method is called Memoization   (top down) 
-// #include<bits/stdc++.h>
-// using namespace std;
-// int f(int n,vector<int> &dp){
-//     if(n<=1)return n;
-//     if(dp[n]!=-1)return dp[n];
+// ! this Method is called Memoization   (top down) 
+#include<bits/stdc++.h>
+using namespace std;
+int f(int n,vector<int> &dp){
+    if(n<=1)return n;
+    if(dp[n]!=-1)return dp[n];
 
-//     return dp[n] = f(n-1,dp) + f(n-2,dp);
-// }
-// int main(){
-//     ios_base::sync_with_stdio(false);
-//     cin.tie(nullptr);
+    return dp[n] = f(n-1,dp) + f(n-2,dp);
+}
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
 
-//     int n;cin>>n;
-//     vector<int> dp(n+1,-1);
-//     cout<<f(n,dp);
+    int n;cin>>n;
+    vector<int> dp(n+1,-1);
+    cout<<f(n,dp);
     
-//     /*
-//     tc o(n)
-//     sc o(n) + o(n)
-//        one for the recurrsion stack space and one for the vector 
+    /*
+    tc o(n)
+    sc o(n) + o(n)
+       one for the recurrsion stack space and one for the vector 
        
-//     */
+    */
 
-//     return 0;
-// }
+    return 0;
+}
 
 
 /* how to convert a memoizatiron to a tabulation 
