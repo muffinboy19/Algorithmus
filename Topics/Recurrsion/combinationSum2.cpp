fac9a65d772fd24db1e845bc27307cf2b5ca72d4
muffinboy <87519281+muffinboy19@users.noreply.github.com>
2024-08-09 18:13:56 +0530

    added the combination 2 code

--- Topics/Recurrsion/combinationSum2.cpp
+++ Topics/Recurrsion/combinationSum2.cpp
@@ -16,6 +16,14 @@ thus now you increase the index
 */
 
 /*
+^ rember in such code u have to also make sure that the duplciates are not there 
+^ hence alwasy right a code to 
+
+for now its unclear why we have to recall the funciotn again in duplciate rewomval but we have to do that 
+
+*/
+
+/*
 dont get confused 
 here the i is for index 
          target is for the tareget sum 
@@ -26,46 +34,41 @@ here the i is for index
 
 
 #include<bits/stdc++.h>
+
 using namespace std;
-void ish(int i,int target,vector<int> &arr,vector<int> &ds,set<vector<int>> &osh){
-    if(i==arr.size()){
-        if(target==0){
-            osh.insert(ds);
-            // for(auto it:ds){cout<<it<<" ";}cout<<endl;
-        }
-        return;
-    }
-
-    /// pickUP the elemtne 
-    if(arr[i]<=target){
-        ds.push_back(arr[i]);
-        ish(i+1,target-arr[i],arr,ds,osh);
-        ds.pop_back();
-    }
-    // do not pic this elment 
-    ish(i+1,target,arr,ds,osh);
+void findCombination(int ind, int target, vector < int > & arr, vector < vector < int >> & ans, vector < int > & ds) {
+  if (target == 0) {
+    ans.push_back(ds);
+    return;
+  }
+  for (int i = ind; i < arr.size(); i++) {
+    if (i > ind && arr[i] == arr[i - 1]) continue;
+    if (arr[i] > target) break;
+    ds.push_back(arr[i]);
+    findCombination(i + 1, target - arr[i], arr, ans, ds);
+    ds.pop_back();
+  }
 }
-int main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-    vector<int> arr={10,1,2,7,6,1,5};
-    vector<int> ds;
-    set<vector<int>> osh;
-    int target=8;
-    sort(arr.begin(),arr.end());
-    ish(0,target,arr,ds,osh);
-    for(auto it:osh){
-        for(auto jt:it){
-            cout<<jt<<" ";
-        }
-        cout<<endl;
-    }
-    cout<<endl;
-    
-
-    return 0;
+vector < vector < int >> combinationSum2(vector < int > & candidates, int target) {
+  sort(candidates.begin(), candidates.end());
+  vector < vector < int >> ans;
+  vector < int > ds;
+  findCombination(0, target, candidates, ans, ds);
+  return ans;
 }
-
+int main() {
+  vector<int> v{10,1,2,7,6,1,5};
+  vector < vector < int >> comb = combinationSum2(v, 8);
+  cout << "[ ";
+  for (int i = 0; i < comb.size(); i++) {
+    cout << "[ ";
+    for (int j = 0; j < comb[i].size(); j++) {
+      cout << comb[i][j] << " ";
+    }
+    cout << "]";
+  }
+  cout << " ]";
+}
 
 
 /*
---
+++ Topics/SlidingWindow/MaxMinInWindow.cpp
@@ -0,0 +1,20 @@
+/*
+In this algo we will leanr how to manipulate the max element or hte min elent in a window throught the arary 
+
+^ min max elment in a window 
+^ here we used deque for a poping and entering system 
+
+also called monotonically decresing queue 
+
+
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+
+    return 0;
+}
