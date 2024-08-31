43d3d85d6145c4cb8bd5a6816e9c2a9acc14c283
muffinboy <87519281+muffinboy19@users.noreply.github.com>
2024-08-31 20:10:33 +0530

    we started graph

---
+++ Topics/Graph/intro.cpp
@@ -0,0 +1,25 @@
+/*
+In this code we will leanrn how to make and use graphs 
+
+
+we will be given 2 numbers
+
+number of nodes and number of edges 
+
+then we will be given m liners where they would contain the edges einfomration
+it will givee u the corrdinates of the nodes / value of hte nodes  that would be  join by the edges 
+
+
+or you will be given the matrix of whch  would coantin the smae info 
+being set on the intersiton of hte nodes that are sopposed to be connected
+
+\
+the nbumnber of  possible edgges for n nodes are 
+            n*(n-1)/2
+
+            and the number of possible graphs which can exitsare 
+
+            2 raised to number of possible edges
+
+            
+*/
