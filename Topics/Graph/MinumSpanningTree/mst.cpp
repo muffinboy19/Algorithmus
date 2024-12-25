1662bbd0953ba035fb9e4313fe6d1b712ebcf154
muffinboy <87519281+muffinboy19@users.noreply.github.com>
2024-12-25 09:59:54 +0530

    disjoit set | MST

---
+++ Topics/Graph/MinumSpanningTree/mst.cpp
@@ -0,0 +1,18 @@
+/*(
+here we would write the code for a minum spaning tree
+definaiton of spanning tree: 
+were we have n nodes n-1 edges
+and all nodes are reacble from each other 
+    - is undirected ( here at least)
+
+
+    ok so what i get it that 
+    there would exits a graph  n we would draw a spaning tree of it 
+    and the spanning tree which would have the minum weight sum  total of all edges 
+    is called the mininum spanning tree
+
+
+    we can find a mst from
+        prunes algo 
+        krunles algo( using disjonint sets).
+*/
